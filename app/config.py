ccc8e7234120ebdfed7f94eab96ae64964337aa0
Serguei <aulasbiertauio@gmail.com>
2026-09-19 17:02:37 -0500

    Subir archivo config.py actualizado con model_accuracy

--- app/config.py
+++ app/config.py
@@ -1,6 +1,7 @@
 class Settings:
-    model_path: str = "app/cnn/best_model.pt"  # <-- RUTA ACTUALIZADA
+    model_path: str = "app/cnn/best_model.pt"
     num_classes: int = 2
+    model_accuracy: str = "72.9% (F1-Macro CV)"
     qdrant_url: str = "http://localhost:6333"
     image_size: int = 224
     qdrant_host: str = "localhost"
